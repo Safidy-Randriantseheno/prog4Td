b29ca7c6607fe317ecb660f1050f319aaf1f0fe5
Safidy-Randriantseheno <hei.safidy.7@gmail.com>
2023-08-07 20:27:20 +0300

    fix: phone validator fixed

---
+++ src/main/java/com/prog4/progtd/Controller/mapper/EmployeeMapper.java
@@ -0,0 +1,4 @@
+package com.prog4.progtd.Controller.mapper.modelView;
+
+public class EmployeeMapper {
+}
--- src/main/java/com/prog4/progtd/model/Validator/PhoneValidator.java
+++ src/main/java/com/prog4/progtd/model/Validator/PhoneValidator.java
@@ -1,5 +1,35 @@
 package com.prog4.progtd.model.Validator;
 
-public class PhoneValidator {
+import com.prog4.progtd.Service.InvalidPhoneNumberException;
+import com.prog4.progtd.model.Phone;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
 
+import javax.xml.validation.Validator;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Consumer;
+
+@Component
+
+public class PhoneValidator implements Consumer<String> {
+    private final Set<String> allowedCountryCodes = new HashSet<>();
+
+    public PhoneValidator() {
+        allowedCountryCodes.add("+261");
+        allowedCountryCodes.add("+33");
+        allowedCountryCodes.add("+1");
+    }
+    @Override
+    public void accept(String phoneNumber) {
+
+        if (phoneNumber == null || ( phoneNumber.length() != 12 && phoneNumber.length() != 13 && phoneNumber.length() != 14)) {
+            throw new InvalidPhoneNumberException("phone number is mandatory" + phoneNumber.length() +"masstay");
+        }
+
+    }
 }
+
+
